0298ba301bb34adfdafad8538a28ecddc68da560
진효찬 <attattahugen@gmail.com>
2025-01-21 18:09:21 +0900

    django (board 작업중)

--- boardapp/board/forms.py
+++ boardapp/board/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Question, Answer
+from .models import Question, Answer, Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
 
 
 class QuestionForm(forms.ModelForm):
